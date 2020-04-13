2209ba30a1b3df049ba4bde8043553dd2f33bebe
DimaMakarenko <makarenko.d.viri@gmail.com>
2020-04-13 20:14:54 +0300

    Task reducer

--- app/screens/Tasks/List/ListTask.tsx
+++ app/screens/Tasks/List/ListTask.tsx
@@ -1,33 +1,53 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { FC } from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+// redux
+import { connect } from 'react-redux';
 import firebase from 'firebase';
 // component
 import Title from '../../../components/Title/Title';
 // styles
 import { basicStyles } from '../../../theme/basicStyles';
 
-const ListTask = () => {
+interface IListTask {
+  tasks: object[];
+}
+
+const ListTask: FC<IListTask> = ({ tasks }) => {
+  console.log(tasks);
   const handlePress = () => {
     firebase.auth().signOut();
   };
   return (
     <View style={basicStyles.container}>
       <View style={styles.header}>
-        <Title text='ListTask' />
+        <Title text='Tasks' />
         <Text onPress={handlePress} style={styles.logOut}>
           Log out
         </Text>
       </View>
+      {tasks.length === 0 ? (
+        <View style={styles.emptyList}>
+          <Text style={styles.emptyListText}>You don’t have tasks recently added.</Text>
+          <Text style={styles.emptyListText}>Generate list of tasks</Text>
+        </View>
+      ) : (
+        <View style={styles.tasks}>
+          <FlatList data={tasks} renderItem={({ item }) => <Text>1 {item}</Text>} />
+        </View>
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   header: { width: '100%', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' },
+  tasks: {},
+  emptyList: { justifyContent: 'center', alignItems: 'center', height: '100%' },
+  emptyListText: { textAlign: 'center' },
   logOut: { color: 'rgba(218, 11, 11,0.6)' },
   text: {
     fontSize: 26,
   },
 });
 
-export default ListTask;
+export default connect((state) => ({ tasks: state.tasks }))(ListTask);
--- app/store/reducers/tasks.ts
+++ app/store/reducers/tasks.ts
@@ -1,12 +1,29 @@
-import { createAction } from '../createAction';
 // const
 const ADD_TASK = 'tasks/ADD_TASK';
 
+//
+interface ISession {
+  start: number;
+  end: number;
+}
+
+interface ITask {
+  id: string;
+  title: string;
+  project: string;
+  duration: number;
+  isPaused: boolean;
+  isDone: boolean;
+  timeSession: ISession[];
+  tags?: string[];
+  file?: any;
+}
+
 // state
-const initialState: object[] = [];
+const initialState: ITask[] = [];
 
 // actions
-export const addTask = createAction(ADD_TASK);
+export const addTask = (payload: ITask) => ({ type: ADD_TASK, payload });
 
 export const tasks = (state = initialState, { type, payload }) => {
   switch (type) {
